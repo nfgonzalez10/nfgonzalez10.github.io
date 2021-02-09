f51a0dbbdb722ee9b1177cfeaa9fabef9f1c5c81
Nicolas Gonzalez <ngonzalez@smarthop.co>
2021-02-09 18:11:16 -0500

    :fire: Removing dist folder and added docs folder

--- webpack.config.js
+++ webpack.config.js
@@ -9,11 +9,11 @@ module.exports = {
 	},
 	output: {
 		filename: "[name].bundle.js",
-		path: path.resolve(__dirname, "dist"),
+		path: path.resolve(__dirname, "docs"),
 	},
 	devtool: "inline-source-map",
 	devServer: {
-		contentBase: "./dist",
+		contentBase: "./docs",
 		port: 9000,
 		open: true,
 	},
